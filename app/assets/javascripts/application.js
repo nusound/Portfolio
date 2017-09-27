cc2600308c5cecf79f4047f02cc760b793d41d83
nusound <uroshni77@gmail.com>
2017-09-27 10:18:48 +0200

    Implemented action cable for comments

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,6 @@
 //
 //= require jquery3
 //= require popper
-//= require bootstrap-sprockets
 //= require jquery_ujs
 //= require jquery-ui
 //= require html.sortable
@@ -22,3 +21,4 @@
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
+//= require bootstrap-sprockets
