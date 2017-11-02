9b7c3bb9b8c649369c24666c96f3775e95182f00
nusound <uroshni77@gmail.com>
2017-11-02 18:04:41 +0100

    Style and content changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,8 +10,8 @@ module ApplicationHelper
   end
 
   def source_helper(styles)
-  	if session[:source]
-  	  greeting = "Thanks for visiting me from #{session[:source]}, please feel free to #{ link _to 'contact me', contact_path } if you'd like to work together."
+    if session[:source]
+      greeting = "Thanks for visiting me from #{session[:source]}, please feel free to #{ link_to 'contact me', contact_path } if you'd like to work together."
       content_tag(:div, greeting.html_safe, class: styles)
     end
   end
