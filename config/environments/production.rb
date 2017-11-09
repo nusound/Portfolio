fe99e284c9a178cb7582cec6f60f57e5a771c8de
nusound <uroshni77@gmail.com>
2017-11-09 16:57:44 +0100

    Integrated production calls for action cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.allowed_request_origins = ['https://nu-portfolio.herokuapp.com', 'http://nu-portfolio.herokuapp.com']
+  config.action_cable.url = "wss://nu-portfolio.herokuapp.com/cable"
 end
